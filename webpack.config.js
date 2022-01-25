b495ef2b59d1e16b355bbd3a8ed1863c23e5e8f7
Susan Cheng <susan.doggie@gmail.com>
2022-01-25 22:51:46 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,9 @@ const babelLoaderConfiguration = {
     loader: 'babel-loader',
     options: {
       presets: [
-        [
-          '@babel/preset-react',
-        ],
+        '@babel/preset-env',
+        '@babel/preset-react',
       ],
-      plugins: [
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-proposal-class-properties',
-      ]
     },
   }
 };
