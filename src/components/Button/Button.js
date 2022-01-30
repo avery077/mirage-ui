e5417eb6df30a8ce2207cfe921c15ef71877f36f
avery077 <doh.cc1995@gmail.com>
2022-01-30 13:28:17 +0800

    revamp button styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,17 @@ import {View, Text, Pressable, StyleSheet} from 'react-native';
 
 function Button(props) {
     return (
-        <Pressable style={[styles.btn, props.style]} onPress={props.onPress}>
+        <Pressable 
+            style={({ pressed }) => [
+                {
+                    boxShadow: pressed
+                        ? "rgb(0 0 0 / 20%) 0px 5px 5px -3px, rgb(0 0 0 / 14%) 0px 8px 10px 1px, rgb(0 0 0 / 12%) 0px 3px 14px 2px"
+                        : "rgb(0 0 0 / 20%) 0px 2px 4px -1px, rgb(0 0 0 / 14%) 0px 4px 5px 0px, rgb(0 0 0 / 12%) 0px 1px 10px 0px"
+                },
+                ,styles.btn ,props.style
+            ]}
+            onPress={props.onPress}
+            >
             <Text style={styles.text}>{props.btnName}</Text>
         </Pressable>
     );
@@ -11,18 +21,21 @@ function Button(props) {
 
   const styles = StyleSheet.create({
       btn:{
-        fontSize: "medium",
-        backgroundColor: "#007bff",
-        borderColor: "#007bff",
-        borderRadius: 5,
-        border: "1px solid transparent",
-        cursor: "pointer",
-        padding: 10,
-        minWidth: 100,
-        textAlign: "center"
-      },
+          backgroundColor: "#1976d2",
+          fontSize: "medium",
+          borderColor: "#007bff",
+          borderRadius: 5,
+          border: "1px solid transparent",
+          cursor: "pointer",
+          padding: 10,
+          minWidth: 100,
+          textAlign: "center",
+          width: "fit-content"
+        },
       text:{
-          color: "#fff"
+          color: "#fff",
+          fontWeight: 600,
+          letterSpacing: 1
       }
   });
   
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,4 +3,9 @@ import {storiesOf} from '@storybook/react-native';
 
 import Button from './Button';
 
-storiesOf('Button', module).add('example', () => <Button onPress={()=>{console.log("okok")}} btnName="just testing.." style={{ backgroundColor: "red" }}/>);
+storiesOf('Button', module).add('example', () => 
+<Button 
+    onPress={()=>{console.log("okok")}} 
+    btnName="submit"
+    // style={{ backgroundColor : "green" }}
+    />);
